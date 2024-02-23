3f12a27a7cdd93795e6a248f9afa800a527aa010
hasankasap <49130933+hasankasap@users.noreply.github.com>
2024-02-23 18:50:56 +0300

    Player controls added

---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : Character
+{
+    private FloatingJoystick joystick;
+    private bool canPlay = false;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+    }
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+    }
+    private void Update()
+    {
+        if (canPlay)
+        {
+            Movement();
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (!navMeshAgent.isStopped)
+                    navMeshAgent.isStopped = true;
+                if (animator != null)
+                {
+                    animator.SetBool("Run", false);
+                }
+            }
+        }
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        joystick = FindObjectOfType<FloatingJoystick>();
+    }
+    protected override void Movement()
+    {
+        base.Movement();
+        if (joystick == null)
+            return;
+        Vector3 movementDir = new Vector3(joystick.Horizontal, 0, joystick.Vertical) * Sensitivity;
+        if (movementDir.magnitude > .05f)
+        {
+            if (navMeshAgent.isStopped)
+                navMeshAgent.isStopped = false;
+            navMeshAgent.SetDestination(transform.position + movementDir);
+            if (animator != null)
+            {
+                animator.SetBool("Run", true);
+            }
+        }
+
+    }
+    protected override void OnGameStarted(object[] obj)
+    {
+        base.OnGameStarted(obj);
+        canPlay = true;
+    }
+}
