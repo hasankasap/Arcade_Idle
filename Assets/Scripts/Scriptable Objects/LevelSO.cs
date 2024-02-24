da16ef111045db00ddb6bd2ac3fe40f4ea2fd294
hasankasap <49130933+hasankasap@users.noreply.github.com>
2024-02-24 06:18:03 +0300

    Update LevelSO.cs

--- Assets/Scripts/Scriptable Objects/LevelSO.cs
+++ Assets/Scripts/Scriptable Objects/LevelSO.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Level", menuName = "ScriptableObjects/Level")]
 public class LevelSO : ScriptableObject
 {
-    public Level levelPrefab;
+    public Level LevelPrefab;
 
     public Level GetLevelPrefab()
     {
-        return levelPrefab;
+        return LevelPrefab;
     }
 }
